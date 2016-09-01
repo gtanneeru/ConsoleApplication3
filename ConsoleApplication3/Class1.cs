ec3058cc6c9ba06148dea7c977abbec511f5daaa
gtanneeru <madhusudan@LIVINGROOM-PC>
2016-09-01 08:54:17 -0400

    Add project files.

---
+++ ConsoleApplication3/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class DiceRoll
+    {
+        public void rollDice(int d1, int d2)
+        {
+            if (d1 == 6 && d2 == 6)
+                Console.WriteLine("Craps");
+            else if (d1 == 1 && d2 == 1)
+                Console.WriteLine("Snake Eyes");
+            else if (d1 == 4 && d2 == 4)
+                Console.WriteLine("Box Cars");
+
+
+        }
+      
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Lab_5
+    {
+
+        static void Main(string[] args)
+        {
+
+            
+            int NumberOfSidedDice;
+            string sides;
+            Console.WriteLine("Welcome to Grand Circus Casino!");
+            Console.WriteLine("Roll the Dice?(y/n)");
+            string play = Console.ReadLine();
+            
+            while (play == "y")
+            {
+
+                Console.WriteLine(" How many sided dice you want to roll? Enter a number");
+                sides = Console.ReadLine();
+                bool q = int.TryParse(sides, out NumberOfSidedDice);
+
+                if (q == true)
+                {
+                    Console.WriteLine("Here u go rolling the dice for you");
+                    Random random = new Random();
+                    int randomNumber = random.Next(1, NumberOfSidedDice);
+                    int anotherNumber = random.Next(1, NumberOfSidedDice);
+
+                    Console.WriteLine(randomNumber);
+                    Console.WriteLine(anotherNumber);
+
+                    DiceRoll rollRoll = new DiceRoll();
+                    rollRoll.rollDice(randomNumber, anotherNumber);
+                }
+                else
+                Console.WriteLine("Enter an Integer Number!");
+
+                Console.WriteLine("Roll the Dice?(y/n)");
+                play = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
